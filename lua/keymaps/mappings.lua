04dfbd871edf246b7ce54df0cc4b7b4d388ded95
Matt Riley <matt@mattriley.me>
2021-08-25 19:06:18 +0100

    Minor improvements
    * Notifies when keymapping fails
    * Saves undo history and backups
    * Made the timeout for plugin notifications shorter

--- lua/keymaps/mappings.lua
+++ lua/keymaps/mappings.lua
@@ -2,7 +2,22 @@ local nvimtree_mappings = require("plugins.nvimtree").keymaps
 local trouble_mappings = require("plugins.trouble").keymaps
 local lsp_mappings = require("settings.lsp.mappings")
 local glow_mappings = require("plugins.glow").keymaps
+local extend_table = vim.tbl_deep_extend
+local has_notify, notify = pcall(require, "notify")
 
-local wk_mappings = vim.tbl_deep_extend("error", nvimtree_mappings, lsp_mappings, trouble_mappings, glow_mappings)
+local has_mappings, wk_mappings = pcall(
+	extend_table,
+	"error",
+	nvimtree_mappings,
+	lsp_mappings,
+	trouble_mappings,
+	glow_mappings
+)
 
-return wk_mappings
+if has_mappings then
+	return wk_mappings
+else
+	if has_notify then
+		notify(table.concat(wk_mappings, "\n"), "error", { title = "Error with the keymaps" })
+	end
+end
