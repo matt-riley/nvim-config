f40dea0937caea360e68b0500cbe510ea93e3b84
Matt Riley <matt@mattriley.me>
2021-11-19 14:30:15 +0000

    Updated the status line
    The right components still need some work but overall it's better than
    it was.

--- lua/plugins/feline/components.lua
+++ lua/plugins/feline/components.lua
@@ -1,4 +1,3 @@
-local lsp = require("feline.providers.lsp")
 local vi_mode = require("feline.providers.vi_mode")
 
 local components = {
@@ -11,50 +10,132 @@ components.active[1][1] = {
 	hl = function()
 		return {
 			fg = vi_mode.get_mode_color(),
-		}
-	end,
-	right_sep = " ",
-}
-
-components.active[1][2] = {
-	provider = function()
-		return vi_mode.get_vim_mode()
-	end,
-	hl = function()
-		return {
-			fg = vi_mode.get_mode_color(),
 			style = "bold",
 		}
 	end,
-	right_sep = " ",
+	right_sep = {
+		str = "",
+		hl = function()
+			return {
+				fg = vi_mode.get_mode_color(),
+			}
+		end,
+	},
 }
 
-components.active[1][3] = {
-	provider = "git_branch",
+components.active[1][2] = {
+	provider = "  ",
 	hl = function()
 		return {
-			fg = "purple",
+			fg = "orange",
+			style = "bold",
 		}
 	end,
 }
+components.active[1][3] = {
+	provider = function()
+		local mode = vi_mode.get_vim_mode():lower()
+		return mode:sub(1, 1):upper() .. mode:sub(2)
+	end,
+	hl = function()
+		return {
+			fg = "bg",
+			bg = vi_mode.get_mode_color(),
+			style = "bold",
+		}
+	end,
+	left_sep = "█",
+	right_sep = "█",
+}
 
 components.active[1][4] = {
-	provider = "git_diff_added",
+	provider = "file_info",
 	hl = {
-		fg = "green",
+		fg = "fg_dark",
+		style = "bold",
+	},
+	right_sep = {
+		str = "",
+		hl = {
+			fg = "fg_dark",
+		},
+	},
+	left_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
 	},
 }
 
-components.active[1][5] = {
-	provider = "git_diff_changed",
-	hl = {
-		fg = "orange",
+components.active[2][1] = {
+	provider = "",
+	hl = function()
+		return {
+			fg = vi_mode.get_mode_color(),
+		}
+	end,
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "blue",
+		},
 	},
 }
-components.active[1][6] = {
-	provider = "git_diff_removed",
+
+components.active[2][2] = {
+	provider = "lsp_client_names",
+	hl = {
+		fg = "blue",
+		style = "bold",
+	},
+	icon = {
+		str = "  ",
+		hl = {
+			fg = "fg",
+		},
+	},
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "blue",
+		},
+	},
+
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "blue",
+		},
+	},
+}
+
+components.active[2][3] = {
+	provider = "",
+	hl = function()
+		return {
+			fg = vi_mode.get_mode_color(),
+		}
+	end,
+	right_sep = {
+		str = "",
+		hl = {
+			fg = "blue",
+		},
+	},
+}
+
+components.active[3][1] = {
+	provider = "diagnostic_errors",
 	hl = {
 		fg = "red",
+		style = "bold",
+	},
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "red",
+		},
 	},
 	right_sep = {
 		str = " ",
@@ -64,57 +145,166 @@ components.active[1][6] = {
 	},
 }
 
-components.active[2][1] = {
-	provider = "lsp_client_names",
-	hl = {
-		fg = "yellow",
-		bg = "bg",
-		style = "bold",
-	},
-	right_sep = " ",
-}
-
-components.active[2][2] = {
-	provider = "diagnostic_errors",
-	enabled = function()
-		return lsp.diagnostics_exist("Error")
-	end,
-	hl = {
-		fg = "red",
-		style = "bold",
-	},
-}
 -- diagnosticWarn
-components.active[2][3] = {
+components.active[3][2] = {
 	provider = "diagnostic_warnings",
-	enabled = function()
-		return lsp.diagnostics_exist("Warning")
-	end,
 	hl = {
 		fg = "yellow",
 		style = "bold",
+	},
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "yellow",
+		},
+	},
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
 	},
 }
 -- diagnosticHint
-components.active[2][4] = {
+components.active[3][3] = {
 	provider = "diagnostic_hints",
-	enabled = function()
-		return lsp.diagnostics_exist("Hint")
-	end,
 	hl = {
 		fg = "cyan",
 		style = "bold",
 	},
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "cyan",
+		},
+	},
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
+	},
 }
 -- diagnosticInfo
-components.active[2][5] = {
+components.active[3][4] = {
 	provider = "diagnostic_info",
-	enabled = function()
-		return lsp.diagnostics_exist("Information")
-	end,
 	hl = {
 		fg = "skyblue",
 		style = "bold",
+	},
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "skyblue",
+		},
+	},
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
+	},
+}
+
+components.active[3][5] = {
+	provider = "git_branch",
+	hl = {
+		fg = "green",
+		style = "bold",
+	},
+	icon = "  ",
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "green",
+		},
+	},
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
+	},
+}
+
+components.active[3][6] = {
+	provider = "git_diff_added",
+	hl = {
+		fg = "green",
+		style = "bold",
+	},
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "green",
+		},
+	},
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
+	},
+}
+
+components.active[3][7] = {
+	provider = "git_diff_changed",
+	hl = {
+		fg = "orange",
+		style = "bold",
+	},
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
+	},
+
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "orange",
+		},
+	},
+}
+components.active[3][8] = {
+	provider = "git_diff_removed",
+	hl = {
+		fg = "red",
+		style = "bold",
+	},
+	right_sep = {
+		str = " ",
+		hl = {
+			fg = "NONE",
+		},
+	},
+	left_sep = {
+		str = "",
+		hl = {
+			fg = "red",
+		},
+	},
+}
+
+components.active[3][9] = {
+	provider = "position",
+	hl = {
+		fg = "bg",
+		bg = "purple",
+		style = "bold",
+	},
+	right_sep = {
+		str = "█",
+		hl = {
+			fg = "purple",
+		},
+	},
+	left_sep = {
+		str = "█",
+		hl = {
+			fg = "purple",
+		},
 	},
 }
 
